5fa43470a0144711f6defa70cce6bc9c9ad96d3e
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-04-27 16:19:02 +0900

    Refactor test_linked_list.hpp and test_queue.hpp to include respective header files

--- lec2/linked_list/test_linked_list.hpp
+++ lec2/linked_list/test_linked_list.hpp
@@ -3,17 +3,7 @@
 #include <string>
 
 extern "C" {
-typedef struct cell {
-  int data;
-  struct cell *next;
-} Cell;
-
-extern Cell *head;
-
-void insert_cell(Cell *p, int d);
-void insert_cell_top(int d);
-void delete_cell(Cell *p);
-void delete_cell_top(void);
+#include "linked_list.h"
 }
 
 void test_insert_cell(Cell *p, int d);
--- lec2/queue/test_queue.hpp
+++ lec2/queue/test_queue.hpp
@@ -3,19 +3,7 @@
 #include <string>
 
 extern "C" {
-typedef struct queue {
-  int *buffer;
-  int length;
-  int front;
-  int rear;
-} Queue;
-
-Queue *create_queue(int len);
-void enqueue(Queue *q, int d);
-int dequeue(Queue *q);
-void display(Queue *q);
-void delete_queue(Queue *q);
-int get_capacity(Queue *q);
+#include "queue.h"
 }
 
 void test_display(Queue *q, std::string &output, const int max_size);
